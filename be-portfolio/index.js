539e58b85954a6e895cb4a301ed09b4c8f11e342
Davaakhuu Enkhzaya <davaakhuuenkhzaya@Davaakhuus-MacBook-Air.local>
2025-07-22 16:55:20 +0800

    connection

--- be-portfolio/index.js
+++ be-portfolio/index.js
@@ -4,6 +4,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { MailRouter } from "./routes/contact.js";
 
+const connect = async () => {
+  await mongoose.connect(
+    "mongodb+srv://enhzaya0111:to9jHSJfU2VaJ1yI@food-delivery.jfs9mpb.mongodb.net/food-delivery"
+  );
+};
+
+connect();
+
 dotenv.config();
 const app = express();
 
@@ -12,7 +20,7 @@ app.use(express.json());
 
 app.use(MailRouter);
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
 const uri = process.env.MONGODB_URI;
 
@@ -20,13 +28,7 @@ if (!uri) {
   console.error("MONGO_URI is not defined");
   process.exit(1);
 }
-mongoose
-  .connect(uri)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(" MongoDB connection error:", err);
-  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
